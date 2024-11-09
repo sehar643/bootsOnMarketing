1b7b274ae478b629648cbe6c1385091ecd63b0a1
sehar643 <seharuzair9@gmail.com>
2024-11-09 18:23:13 +0500

    m

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
   return (
     <div>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        {/* Apply padding only on medium and larger screens */}
+        
         <div className="max-w-screen-xl flex items-center justify-between mx-auto md:px-14">
-          {/* Toggler Button - Order 1 on mobile */}
+         
           <button 
             data-collapse-toggle="navbar-sticky" 
             type="button" 
@@ -20,12 +20,10 @@ const Navbar = () => {
             </svg>
           </button>
 
-          {/* Logo - Order 2 on mobile */}
           <a href="https://flowbite.com/" className="order-2 md:order-1 flex items-center space-x-3 rtl:space-x-reverse">
             <img src="/src/assets/images/logo.jpg" className="h-16 md:h-24" alt="Flowbite Logo" />
           </a>
 
-          {/* Get A Quote Button - Order 3 on mobile */}
           <button 
             type="button" 
             className="order-3 md:order-3 text-white bg-black font-medium rounded-lg text-lg px-4 py-2 text-center"
@@ -33,7 +31,6 @@ const Navbar = () => {
             Get A Quote
           </button>
 
-          {/* Navbar Links - Visible on larger screens */}
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
